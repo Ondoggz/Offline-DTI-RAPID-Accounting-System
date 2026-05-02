5b907376e4481c6a60dd3c6e441af614f19a547d
Nicole <20240031277@my.xu.edu.ph>
2026-05-02 11:27:28 +0800

    Add createdAt field to user object in auth middleware

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -28,6 +28,7 @@ export const protect = async (req, res, next) => {
       id: user._id,
       username: user.username,
       role: user.role,
+      createdAt: user.createdAt,
     };
 
     next();
